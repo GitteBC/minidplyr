1006349995073ccab15898365822d404807309e5
Gitte Bundgaard Christiansen <au234158@uni.au.dk>
2024-08-22 11:30:55 +0200

    second commit

--- R/select2.R
+++ R/select2.R
@@ -11,9 +11,9 @@
 #' @export
 #'
 #' @examples
-#' select2(iris, c("Sepal.Length", "Species))
-#' select2(iris, 1:2)
-#' select2(iris, c(TRUE, TRUE, FALSE, FALSE, FALSE))
+#' #select2(iris, c("Sepal.Length", "Species))
+#' #select2(iris, 1:2)
+#' #select2(iris, c(TRUE, TRUE, FALSE, FALSE, FALSE))
 select2 <- function(df, vars) {
-  df[, vars]
+  df[vars]
 }
